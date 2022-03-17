f1349568ee9d2614fd472a10ef52ed8d203b3586
DEV04 <dev04@ADSI0225.local>
2022-03-17 09:37:24 +0800

    removed authorization header in casts controller for testing

--- app/controllers/casts_controller.rb
+++ app/controllers/casts_controller.rb
@@ -1,5 +1,5 @@
 class CastsController < ApplicationController
-    before_action :authenticate_admin!, only: [:create, :update, :destroy]
+    # before_action :authenticate_admin!, only: [:create, :update, :destroy]
 
     def index
         casts = CastsFacade.new
--- db/schema.rb
+++ db/schema.rb
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 2022_03_09_061321) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "admins", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
+  create_table "admins", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
