6a7f80b475dcf41125be6ea2f4ba54ab4850029e
DEV04 <dev04@ADSI0225.local>
2022-03-18 14:20:38 +0800

    secret key of admin

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -25,7 +25,8 @@ class AdminsController < ApplicationController
 
     private
         def get_admin_from_token
-            jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1], ENV['DEVISE_JWT_SECRET_KEY']).first
+            jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1], "d52ef6ba1c804a5cfcbb00c3ad17103339209e8d64f46e4d98e173b0c2b4c3ace718dd7a31e43c6fa239592d29d10c7b5fdcf1f354eead1709285638e324d85c").first
+            # jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1], "b40fd77db45d6a45348d0c0de0f6ab8ebf16aafe3207d952fbad14e71ed672e1d50a6a6c58ce8de5d04392b17dc1a9251850e602b0cccfb176257f904df8b1bf").first
             admin_id = jwt_payload['sub']
             admin = Admin.find(admin_id.to_s)
         end
